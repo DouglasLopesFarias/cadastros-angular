8969d8ef032595740d447dc3f89738e2cd7c7b94
douglaslopesfarias <douglaslopesfarias@outlook.com>
2024-08-05 19:49:46 -0300

    inclusao aula 30 julho

---
+++ src/app/componentes/cidade-form/cidade-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cidade-form',
+  templateUrl: './cidade-form.component.html',
+  styleUrl: './cidade-form.component.css'
+})
+export class CidadeFormComponent implements OnInit{
+
+  ngOnInit(): void {
+  
+  }
+
+}
